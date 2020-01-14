54612fa9463d3b4924ce944a09c757910def1cde
yseul <qlcid1@gmail.com>
2020-01-14 20:46:03 +0900

    url path parameter

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 var fs = require('fs');
 var template = require('./lib/template.js');
+var path = require('path');
+var sanitizeHtml = require('sanitize-html');
 
 // app.get is for routing
 app.get('/', (req, res) => {
-  fs.readdir('./data', function(error, filelist) {
+  fs.readdir('./data', function(err, filelist) {
     var title = 'Welcome';
     var description = 'Hello, Node.js';
     var list = template.list(filelist);
@@ -18,7 +20,28 @@ app.get('/', (req, res) => {
 })
 
 app.get('/page/:pageId', (req, res) => {
-  res.send(req.params)
+  fs.readdir('./data', function(err, filelist){
+    var filteredId = path.parse(req.params.pageId).base;
+    console.log(filteredId);
+    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+      var title = req.params.pageId;
+      var sanitizedTitle = sanitizeHtml(title);
+      var sanitizedDescription = sanitizeHtml(description, {
+        allowedTags:['h1']
+      });
+      var list = template.list(filelist);
+      var html = template.HTML(sanitizedTitle, list,
+        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+        ` <a href="/create">create</a>
+          <a href="/update?id=${sanitizedTitle}">update</a>
+          <form action="delete_process" method="post">
+            <input type="hidden" name="id" value="${sanitizedTitle}">
+            <input type="submit" value="delete">
+          </form>`
+      );
+      res.send(html);
+    });
+  });
 })
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
